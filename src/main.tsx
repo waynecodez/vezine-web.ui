05324b4019f08dabc8c1f11f89926ad5bb700f41
Wayne P <wayne@iplayulisten.com>
2023-05-23 00:38:57 -0400

    removes: application entry rendering

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,13 @@ export * from './stories/Loader';
 /**
  * React application root
  */
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App.tsx';
+// import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// 	<React.StrictMode>
+// 		<App />
+// 	</React.StrictMode>
+// );
